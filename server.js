c1bf6c9295f5b337e6499deda3a04d2430f1179e
Sandy Hung <sandyhung83@yahoo.com.tw>
2023-02-06 23:19:37 +1100

    added route for main and notes page

--- server.js
+++ server.js
@@ -2,3 +2,24 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
+const { parse } = require("path");
+
+// middleware
+app.use(express.static("public"));
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// routes
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+})
+
+app.listen(3001, () => {
+  console.log("Server is now online!");
+});
