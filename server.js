fa249e1a5f15580694e0b0c32c19c1fc35984e4f
Sandy Hung <sandyhung83@yahoo.com.tw>
2023-02-09 18:24:22 +1100

    updated package.json

--- server.js
+++ server.js
@@ -60,9 +60,9 @@ app.delete("/api/notes/:id", (req,res) => {
   }); 
 })
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-})
+app.get("*", (req, res) => 
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+)
 
 app.listen(PORT, () => {
   console.log("Server is now online!");
